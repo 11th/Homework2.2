f927ef0f312317acf4b1e66afd879e9977498205
Edinichkin <11th@bk.ru>
2022-09-28 22:53:03 +0300

    Homework is done

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,4 @@
+import java.beans.Introspector;
 import java.util.Calendar;
 
 public class Human {
@@ -6,26 +7,65 @@ public class Human {
     private String town;
     private String job;
 
-    public Human(){
-        this("Не указано", 0, "Не указано", "Не указано");
+    public Human(String name, int age, String town){
+        this(name, age, town, null);
     }
 
     public Human(String name, int age, String town, String job){
-        this.name = name;
-        this.town = town;
-        if (age >= 0) {
-            this.yearOfBirth = Calendar.getInstance().get(Calendar.YEAR) - age;
-        } else {
-            this.yearOfBirth = Calendar.getInstance().get(Calendar.YEAR);
+        if (name != null && !name.isEmpty() && !name.isBlank()) {
+            this.name = name;
         }
-        this.job = job;
+        if (town != null && !town.isEmpty() && !town.isBlank()) {
+            this.town = town;
+        }
+        if (job != null && !job.isEmpty() && !job.isBlank()) {
+            this.job = job;
+        }
+        setYearOfBirth(age);
+        setTown(town);
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int age) {
+        if (age > 0) {
+            this.yearOfBirth = Calendar.getInstance().get(Calendar.YEAR) - age;
+        }
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        if (town != null && !town.isEmpty() && !town.isBlank()) {
+            this.town = town;
+        }
     }
 
     @Override
     public String toString() {
+        String name = this.name;
+        if (name == null){
+            name = "Не указано";
+        }
+        String town = this.town;
+        if (town == null){
+            town = "Не указано";
+        }
+        String year = Integer.toString(this.yearOfBirth);
+        if (this.yearOfBirth == 0){
+            year = "Не указано";
+        }
+        String job = this.job;
+        if (job == null){
+            job = "Не указано";
+        }
         return String.format(
-                "Привет! Меня зовут %s. Я из города %s. Я родился в %d году. Я работаю на должности %s. Будем знакомы!"
-                ,name, town, yearOfBirth, job
+                "Привет! Меня зовут %s. Я из города %s. Я родился в %s году. Я работаю на должности %s. Будем знакомы!",
+                name, town, year, job
         );
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import Transport.Car;
+
 public class Main {
     public static void main(String[] args) {
         Human maksim = new Human("Максим", 35, "Минск", "бренд-менеджер");
@@ -8,8 +10,47 @@ public class Main {
         System.out.println(katya);
         Human artem = new Human("Артем", 27, "Москва", "директор по развитию бизнеса");
         System.out.println(artem);
-
-        Human noname = new Human();
+        Human vova = new Human("Владимир", 21, "Казань");
+        System.out.println(vova);
+        Human noname = new Human(null, 0, null);
         System.out.println(noname);
+
+        System.out.println("***");
+
+        Car lada = new Car("Lada", "Grande", 2015, "Россия", "желтый", 1.7, "", 0);
+        System.out.println(lada);
+        Car audi = new Car("Audi", "A8", 2020, "Германия", "черный", 3.0, "", 0);
+        System.out.println(audi);
+        Car bmw = new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0, "", 0);
+        System.out.println(bmw);
+        Car kia = new Car("Kia", "Sportage", 2018, "Южная Корея", "крвсный", 2.4, "", 0);
+        System.out.println(kia);
+        if (!kia.checkInsurance()){
+            System.out.println("Kia: Продлите страховку");
+        }
+        Car hyndai = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевый", 1.6, "", 0);
+        System.out.println(hyndai);
+        hyndai.setRegNumber("Х777ХХ777");
+        hyndai.changeTyres();
+        Car.Insurance insurance = hyndai.new Insurance(365, 5000, "XXX12454");
+        hyndai.setInsurance(insurance);
+        if (!hyndai.checkInsurance()){
+            System.out.println("Hyndai: Продлите страховку");
+        }
+        if (!hyndai.checkInsuranceNumber()){
+            System.out.println("Hyndai: Некорректный номер страховки");
+        }
+        System.out.println(hyndai);
+
+        System.out.println("***");
+
+        Flower rose = new Flower("красный", "Голландия", 35.59);
+        System.out.println(rose);
+        Flower chrysanthemum = new Flower("синий", "", 15, 5);
+        System.out.println(chrysanthemum);
+        Flower peony = new Flower("розовый", "Англия", 15, 5);
+        System.out.println(peony);
+        Flower gypsophila = new Flower("желтый", "Ткрция", 19.5, 10);
+        System.out.println(gypsophila);
     }
 }
